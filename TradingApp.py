a9b1c8de15c7ba52cd1191a6a64765c3204f9db8
Muskan Gupta <67264445+Muskan40@users.noreply.github.com>
2025-09-07 22:23:47 +0530

    Update TradingApp.py

--- TradingApp.py
+++ TradingApp.py
@@ -9,7 +9,7 @@ image_path = os.path.join(base_path,"app.jpg")
 st.set_page_config(page_title="Trading App", page_icon="heavy_dollar_sign:", layout='wide')
 st.title("Trading Guide App :bar_chart:")
 st.header("We provide you with the best trading strategies and tools to maximize your profits!")
-st.image(image_path, use_column_width=True)
+st.image(image_path, use_container_width=True)
 
 st.markdown("## We provide the following services:")
 
@@ -21,3 +21,4 @@ st.write("You can explore predicted closing prices for the next 30days based on 
 
 st.markdown("### :three: CAPM Return")
 st.write("This page allows you to calculate the expected return of a stock based on its risk and the market's expected return using the Capital Asset Pricing Model (CAPM).")
+
